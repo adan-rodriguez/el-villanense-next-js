e3e400676b18995fd246efca867817d2de18766f
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2023-02-03 13:31:13 -0300

    Add Favicons

--- app/head.jsx
+++ app/head.jsx
@@ -10,24 +10,29 @@ export default function HeadHome() {
         name="description"
         content="Todas las noticias de Villa Ana y las noticias más importantes de la región, de la provincia de Santa Fe, de la Argentina y del mundo."
       />
-      {/* <link
+      <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="icons/logo/apple-touch-icon.png"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="icons/logo/favicon-32x32.png"
+        href="/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="icons/logo/favicon-16x16.png"
-      /> */}
-      {/* <link rel="manifest" href="/site.webmanifest" /> */}
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+      <meta name="apple-mobile-web-app-title" content="El Villanense" />
+      <meta name="application-name" content="El Villanense" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#131313"></meta>
       {/* <link rel="canonical" href="https://www.elvillanense.com.ar/" /> */}
       <meta property="og:title" content="El Villanense" />
       <meta
