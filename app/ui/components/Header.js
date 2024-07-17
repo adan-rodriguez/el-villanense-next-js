048e363759b5c69a563b816a1b5fbdb2ff961a5c
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2024-07-17 13:26:56 -0300

    Quito menú

--- app/ui/components/Header.js
+++ app/ui/components/Header.js
@@ -2,9 +2,7 @@
 
 import styles from "../styles/Header.module.css";
 import Logo from "./Logo";
-import Image from "next/image";
-import useMenu from "@/app/hooks/useMenu";
-import PhoneMenu from "./PhoneMenu";
+// import useMenu from "@/app/hooks/useMenu";
 import AuthorImage from "./AuthorImage";
 import { logout } from "@/app/lib/auth";
 import useMenuUser from "@/app/hooks/useMenuUser";
@@ -24,7 +22,7 @@ import { AuthContext } from "@/app/context/auth";
 // ];
 
 export default function Header() {
-  const { isMenuOpen, getIsMenuOpen } = useMenu();
+  // const { isMenuOpen, getIsMenuOpen } = useMenu();
   const { isMenuUserOpen, getIsMenuUserOpen } = useMenuUser();
   const { user } = useContext(AuthContext);
 
@@ -48,69 +46,50 @@ export default function Header() {
     <header className={styles.header}>
       <div className={styles.header_container}>
         <Logo />
-        {!isMenuOpen ? (
-          <div className={styles.userandopenbutton_container}>
-            {user && (
-              <div className={styles.buttonandmenu_user_container}>
-                <button
-                  className={styles.openmenuuser_button}
-                  onClick={() => getIsMenuUserOpen(!isMenuUserOpen)}
+        {user && (
+          <div className={styles.buttonandmenu_user_container}>
+            <button
+              className={styles.openmenuuser_button}
+              onClick={() => getIsMenuUserOpen(!isMenuUserOpen)}
+            >
+              <AuthorImage src={user.image} author={user.name} />
+            </button>
+            {isMenuUserOpen && (
+              <div className={styles.menuuser_container}>
+                <span className={styles.username}>{user.name}</span>
+                <Link
+                  className={styles.dashboard_link}
+                  onClick={() => getIsMenuUserOpen(false)}
+                  href="/dashboard"
                 >
-                  <AuthorImage src={user.image} author={user.name} />
+                  Dashboard
+                </Link>
+                <Link
+                  className={styles.new_link}
+                  onClick={() => getIsMenuUserOpen(false)}
+                  href="/dashboard/nuevo"
+                >
+                  Nuevo
+                </Link>
+                <Link
+                  className={styles.account_link}
+                  onClick={() => getIsMenuUserOpen(false)}
+                  href="/dashboard/cuenta"
+                >
+                  Cuenta
+                </Link>
+                <button
+                  className={styles.logout_button}
+                  onClick={() => {
+                    getIsMenuUserOpen(false);
+                    logout();
+                  }}
+                >
+                  Cerrar sesión
                 </button>
-                {isMenuUserOpen && (
-                  <div className={styles.menuuser_container}>
-                    <span className={styles.username}>{user.name}</span>
-                    <Link
-                      className={styles.dashboard_link}
-                      onClick={() => getIsMenuUserOpen(false)}
-                      href="/dashboard"
-                    >
-                      Dashboard
-                    </Link>
-                    <Link
-                      className={styles.new_link}
-                      onClick={() => getIsMenuUserOpen(false)}
-                      href="/dashboard/nuevo"
-                    >
-                      Nuevo
-                    </Link>
-                    <Link
-                      className={styles.account_link}
-                      onClick={() => getIsMenuUserOpen(false)}
-                      href="/dashboard/cuenta"
-                    >
-                      Cuenta
-                    </Link>
-                    <button
-                      className={styles.logout_button}
-                      onClick={() => {
-                        getIsMenuUserOpen(false);
-                        logout();
-                      }}
-                    >
-                      Cerrar sesión
-                    </button>
-                  </div>
-                )}
               </div>
             )}
-            <button
-              type="button"
-              onClick={() => getIsMenuOpen(!isMenuOpen)}
-              className={styles.openmenu_button}
-              aria-label="Abrir menú"
-            >
-              <Image
-                src="/icons/menu/openmenu-icon.svg"
-                alt="Abrir menú"
-                width={48}
-                height={48}
-              />
-            </button>
           </div>
-        ) : (
-          <PhoneMenu isMenuOpen={isMenuOpen} getIsMenuOpen={getIsMenuOpen} />
         )}
       </div>
     </header>
--- app/ui/components/PhoneMenu.js
+++ app/ui/components/PhoneMenu.js
@@ -1,23 +1,9 @@
-import Image from "next/image";
 import styles from "../styles/Header.module.css";
 import Footer from "./Footer";
 
-export default function PhoneMenu({ isMenuOpen, getIsMenuOpen }) {
+export default function PhoneMenu(/*{ isMenuOpen, getIsMenuOpen }*/) {
   return (
     <div className={styles.menu_phone}>
-      <button
-        type="button"
-        onClick={() => getIsMenuOpen(!isMenuOpen)}
-        className={styles.closemenu_button}
-        aria-label="Cerrar menú"
-      >
-        <Image
-          src="/icons/menu/closemenu-icon.svg"
-          alt="Cerrar menú"
-          width={48}
-          height={48}
-        />
-      </button>
       <Footer />
     </div>
   );
